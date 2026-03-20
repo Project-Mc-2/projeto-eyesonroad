2c3604c3864a735b3c21a56ad04094e8f3f2d9d8
valentinasenger <dev.valentina.spinelli@gmail.com>
2026-03-20 09:38:20 -0300

    Commit inicial
    Criação de repositórios

---
+++ backend/eyesonroad/eyesonroad/src/main/java/br/com/eyesonroad/api/EyesonroadApplication.java
@@ -0,0 +1,13 @@
+package br.com.eyesonroad.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyesonroadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyesonroadApplication.class, args);
+	}
+
+}
