ad5d8ae7f27cb475e03a6c9e994314adef460e2f
mnorbutas <mnorbutass@gmail.com>
2026-05-13 12:58:01 -0300

    Back atualizado com service e nova entidade

--- backend/eyesonroad/src/main/java/com/projeto/eyesonroad/entities/StatusSono.java
+++ backend/eyesonroad/src/main/java/com/projeto/eyesonroad/entities/StatusSono.java
@@ -2,4 +2,41 @@ package com.projeto.eyesonroad.entities;
 
 public class StatusSono {
 
+	private Long idStatusSono;
+	private String cochilo;
+	private String sono;
+	private String desmaio;
+	
+	public StatusSono() {}
+	public StatusSono(String cochilo, String sono, String desmaio) {
+		this.cochilo = cochilo;
+		this.sono = sono;
+		this.desmaio = desmaio;
+	}
+	public String getCochilo() {
+		return cochilo;
+	}
+	public void setCochilo(String cochilo) {
+		this.cochilo = cochilo;
+	}
+	public String getSono() {
+		return sono;
+	}
+	public void setSono(String sono) {
+		this.sono = sono;
+	}
+	public String getDesmaio() {
+		return desmaio;
+	}
+	public void setDesmaio(String desmaio) {
+		this.desmaio = desmaio;
+	}
+	public Long getIdStatusSono() {
+		return idStatusSono;
+	}
+	public void setIdStatusSono(Long idStatusSono) {
+		this.idStatusSono = idStatusSono;
+	}
+	
+	
 }
---
+++ backend/eyesonroad/src/main/java/com/projeto/eyesonroad/services/EventoService.java
@@ -0,0 +1,5 @@
+package com.projeto.eyesonroad.services;
+
+public class EventoService {
+
+}
