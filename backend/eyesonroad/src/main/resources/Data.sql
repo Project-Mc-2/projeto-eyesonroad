f952a144592543c0f63f3a3f9cc3f81618b8a93a
mnorbutas <mnorbutass@gmail.com>
2026-06-12 10:27:37 -0300

    Back com MySQL
    Aplicando usuários pelo data.sql

---
+++ backend/eyesonroad/src/main/resources/Data.sql
@@ -0,0 +1,20 @@
+INSERT INTO tb_usuarios (
+    nome,
+    login,
+    email,
+    telefone,
+    cpf,
+    data_nascimento,
+    senha,
+    tipo_usuario
+)
+VALUES (
+    'Maria',
+    'maria123',
+    'maria@email.com',
+    '(15)99999-9999',
+    '12345678909',
+    '2000-01-01',
+    'SENHA-1234-5678',
+    'ADMIN'
+);
