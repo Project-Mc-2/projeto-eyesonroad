c4ce7a4023f5e818a9f678265f628085ec9cc5bb
pietragurgell <dev.pietra.gurgel@gmail.com>
2026-06-12 09:37:01 -0300

    atualizações cadastro.js, login.js

--- frontend/Js/cadastro.js
+++ frontend/Js/cadastro.js
@@ -1,66 +1,50 @@
-
-
 const form = document.getElementById("loginForm");
 const erroMsg = document.getElementById("erroMsg");
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
 
-   
     const usuario = document.getElementById("usuario").value.trim();
     const senha = document.getElementById("senha").value.trim();
     const email = document.getElementById("email").value.trim();
     const cpf = document.getElementById("cpf").value.trim();
     const placa = document.getElementById("placa").value.trim();
 
-    
     erroMsg.textContent = "";
 
-    
-    if (
-        usuario === "" ||
-        senha === "" ||
-        email === "" ||
-        cpf === "" ||
-        placa === ""
-    ) {
+    if (!usuario || !senha || !email || !cpf || !placa) {
         erroMsg.textContent = "Preencha todos os campos.";
         return;
     }
 
-    
     if (!email.includes("@") || !email.includes(".")) {
-    erroMsg.textContent = "Digite um email válido.";
-    return;
-}
-
-    if (!emailValido.test(email)) {
-        erroMsg.textContent = "Digite um email válido.";
+        erroMsg.textContent = "Digite um e-mail válido.";
         return;
     }
 
-    
-    let cpfLimpo = cpf;
-    cpfLimpo = cpfLimpo.replaceAll(".", "");
-    cpfLimpo = cpfLimpo.replaceAll("-", "");
+    const cpfLimpo = cpf.replace(/\D/g, "");
 
     if (cpfLimpo.length !== 11) {
         erroMsg.textContent = "CPF inválido.";
         return;
     }
 
-
     if (placa.length < 7) {
-    erroMsg.textContent = "Placa inválida.";
-    return;
-}
-
-    if (!placaValida.test(placa)) {
         erroMsg.textContent = "Placa inválida.";
         return;
     }
 
-   
+    const usuarioCadastro = {
+        usuario,
+        senha,
+        email,
+        cpf,
+        placa
+    };
+
+    localStorage.setItem("usuarioCadastro", JSON.stringify(usuarioCadastro));
+
     alert("Cadastro realizado com sucesso!");
 
+    window.location.href = "../pages/logarConta.html";
 });
