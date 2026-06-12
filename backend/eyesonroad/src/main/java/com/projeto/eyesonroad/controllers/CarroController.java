f029444892f1c655d8fbeb160a40d5a376af926a
mnorbutas <mnorbutass@gmail.com>
2026-06-12 07:30:40 -0300

    atualizações de back

--- backend/eyesonroad/src/main/java/com/projeto/eyesonroad/controllers/CarroController.java
+++ backend/eyesonroad/src/main/java/com/projeto/eyesonroad/controllers/CarroController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projeto.eyesonroad.entities.Carro;
 import com.projeto.eyesonroad.services.CarroService;
 
 import jakarta.validation.Valid;
